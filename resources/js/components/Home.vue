f6cec4d47b142284a54f3d71901a7965a6fd6093
hudipo <dannyhp2016@gmail.com>
2019-11-13 10:39:16 +0700

    testing

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Responsive Hover Table</h3>
+
+                        <div class="card-tools">
+                            <button class="btn btn-success" data-toggle="modal" data-target="#addNew"
+                                   >Add New
+                                <i class="fas fa-user-plus fa-fw"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>User</th>
+                                <th>Date</th>
+                                <th>Status</th>
+                                <th>Reason</th>
+                                <th>Modify</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>183</td>
+                                <td>John Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-success">Approved</span></td>
+                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                                <td>
+                                    <a href="#" >
+                                        <i class="fa fa-edit blue"></i>
+                                    </a>
+                                    /
+                                    <a href="#">
+                                        <i class="fa fa-trash red"></i>
+                                    </a>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog"
+             \aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="AddNewLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form @submit.prevent="createUser">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input v-model="form.name" type="text" name="name"
+                                       placeholder="Name"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                                <has-error :form="form" field="name"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <input v-model="form.email" type="email" name="email"
+                                       placeholder="Email Address"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+                            <div class="form-group">
+                                         <textarea v-model="form.bio" name="bio" id="bio"
+                                                   placeholder="Short bio for user (Optional)"
+                                                   class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                                <has-error :form="form" field="bio"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                    <option value="">Select User Role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Standard User</option>
+                                    <option value="author">Author</option>
+                                </select>
+                                <has-error :form="form" field="type"></has-error>
+                            </div>
+                            <!--                                Pass-->
+                            <div class="form-group">
+                                <input v-model="form.password" type="password" name="password" id="password"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button   type="submit" class="btn btn-primary">Create</button>
+                        </div>
+                    </form>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!--    Modal-->
+
+</template>
+
+<script>
+    import Form from "vform/src/Form";
+
+    export default {
+        data(){
+            return{
+                form : new Form({
+                    id : '',
+                    name : '',
+                    email: '',
+                    password:'',
+                    type:'',
+                    bio:'',
+                    photo:''
+                })
+            }
+        },
+        loadUsers(){
+                axios.get("api/user").then(({ data }) => (this.users = data));
+
+        },
+        methods:{
+            createUser(){
+                this.form.post('api/user');
+            }
+        },
+
+        created() {
+            this.loadUsers();
+        }
+    }
+</script>
